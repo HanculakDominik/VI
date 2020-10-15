4a6cc7a30e2f4313f8d7505c75959623c019d95b
Hanculak <dominik.hanculak@gmail.com>
2020-10-16 00:54:29 +0200

    added title parser

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,14 +7,17 @@ import java.util.regex.Pattern;
 
 //regex: (?!\[\[[^#]*\]\])\[\[[^\|]*#[^\]]*]]
 public class Main {
-
+    static String title;
     static void parserRedirectov() {
         try {
             File myObj = new File("skratenaVerzia.xml");
             Scanner myReader = new Scanner(myObj);
             while (myReader.hasNextLine()) {
                 String data = myReader.nextLine();
-                String regex = "(?!\\[\\[[^#]*\\]\\])\\[\\[[^\\|]*#[^\\]]*]]";
+                if(data.contains("<title>")) {
+                    title = data;
+                }
+                String regex = "(?!\\[\\[[^#]*\\]\\])\\[\\[[^\\|]*#[^\\]]+]]";
 
                 Pattern pattern = Pattern.compile(regex);
                 Matcher matcher
@@ -22,7 +25,14 @@ public class Main {
                         .matcher(data);
 
                 while (matcher.find()) {
+
+                    if(matcher.group().indexOf("#") == 2) {
+                        title = title.replace("<title>", "")
+                                .replace("</title>", "").trim();
+                        System.out.println(title);
+                    }
                     System.out.println(matcher.group());
+
                 }
             }
             myReader.close();
