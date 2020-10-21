416b2481bb48abcb8d571b6950a699f997d26403
Hanculak <dominik.hanculak@gmail.com>
2020-10-21 17:53:49 +0200

    added sections parser

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -91,8 +91,68 @@ public class Main {
         linkedPages.add(new Page(pageTitle,sectionName));
         return linkedPages.size() - 1;
     }
+    private static void sections(){
+        try {
+            File myObj = new File("skratenaVerzia.xml");
+            Scanner myReader = new Scanner(myObj);
+            String regex = "==.+==";
+            Pattern pattern = Pattern.compile(regex);
+            String sectionName;
+            String text = "";
+            Page linkedPage = null;
+            boolean write = false;
+
+            while (myReader.hasNextLine()) {
+                String data = myReader.nextLine();
+                Matcher matcher = pattern.matcher(data);
+                if (data.contains("<title>")) {
+                    linkedPage = null;
+                    String title = data.replace("<title>", "")
+                            .replace("</title>", "").trim();
+                    for (Page var : linkedPages) {
+                        if (var.title.equals(title)) {
+                            linkedPage = var;
+                            break;
+                        }
+                    }
+                }
+                if (linkedPage != null) {
+                    if (matcher.find()) {
+                        if (write){
+                            System.out.println(text);
+                            text = "";
+                        }
+                        write = false;
+                        sectionName = matcher.group()
+                                .replace("=====", "").replace("====", "")
+                                .replace("===", "").replace("==", "").trim();
+                        for (String var: linkedPage.sections) {
+                            if (var.equals(sectionName)) {
+                                write = true;
+                                System.out.println(linkedPage.title + "#" + sectionName);
+                                break;
+                            }
+                        }
+                    } else if (write) {
+                        if (data.startsWith("{{") || data.startsWith("[[Kategória:")){
+                            write = false;
+                            System.out.println(text);
+                            text = "";
+                        } else {
+                            text += data;
+                        }
+                    }
+                }
+
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+    }
     public static void main(String[] args) {
         parserRedirectov();
+        sections();
     }
 }
 
